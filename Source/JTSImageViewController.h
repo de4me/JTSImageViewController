305d162b5d27c1b235cdc8c3df9bf38f5fd368f9
Denis Popov <info@de4me.com>
2018-07-28 00:06:05 +0300

    Исправление ошибки при сборки framework'а;

--- Source/JTSImageViewController.h
+++ Source/JTSImageViewController.h
@@ -8,6 +8,14 @@
 
 @import UIKit;
 
+//! Project version number for JTSImageViewController.
+FOUNDATION_EXPORT double JTSImageViewControllerVersionNumber;
+
+//! Project version string for JTSImageViewController.
+FOUNDATION_EXPORT const unsigned char JTSImageViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTSImageViewController/PublicHeader.h>
+
 #import "JTSImageInfo.h"
 
 ///--------------------------------------------------------------------------------------------------------------------
